2ac5c986623b68d7274d318fb61bb5239d7f33ae
obidikejekwu <obidike.jekwu@gmail.com>
2023-02-18 03:29:23 +0100

    all questions solved, tested and passed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,7 +2,7 @@
 
 /**
  * main - program that prints the lowercase alphabet in reverse.
- * You can only use the putchar function     
+ * You can only use the putchar function
  * Return: 0
  */
 
@@ -12,7 +12,7 @@ int n = 122;
 while (n > 96)
 {
 putchar(n);
-n -= 1;
+n--;
 }
 putchar('\n');
 return (0);
