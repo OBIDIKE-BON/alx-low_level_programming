c3270eeda425db9b94fb1020a7de875b5fbcea47
obidikejekwu <obidike.jekwu@gmail.com>
2023-02-18 04:03:40 +0100

    all questions solved, tested and passed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
  * main - program that prints all possible different combinations of two digits
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1  
+ * 01 and 10 are considered the same combination of the two digits 0 and 1
  * Return: 0
  */
 
