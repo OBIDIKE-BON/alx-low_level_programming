9ea2933df8ff5bb8dbe8c1f4526826664a98a774
obidikejekwu <obidike.jekwu@gmail.com>
2023-02-18 04:05:58 +0100

    all questions solved, tested and passed

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,7 +5,7 @@
  * three digits
  * Numbers must be separated by ,followed by a space
  * The two digits must be different
- * 012, 120, 102, 021, 201, 210 are considered the same combination 
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
  * Return: 0
  */
 
