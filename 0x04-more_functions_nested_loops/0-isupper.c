418e9d925875bc12052266f0e352189341ca0356
obidikejekwu <obidike.jekwu@gmail.com>
2023-02-23 13:33:01 +0100

    Write a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Check if paramter c represents an uppercase letter.
+ * @c: Int representing a character
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	return c >= 65 && c <= 90;
+}
