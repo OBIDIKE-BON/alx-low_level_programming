dceab4dd283f0a09b3ffe825acb5fc3c1fdf4295
obidikejekwu <obidike.jekwu@gmail.com>
2023-03-14 00:26:17 +0100

    Write a program that prints all arguments it receives.

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Description: main - prints all command line arguments, one per line.
+ * followed by a new line.
+ * @argc: the number of cammand line arguments passed at run-time.
+ * @argv: an array of cammand line arguments passed at run-time.
+ * Return: 0 if success.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+	UNUSED(argv);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 # THE PROJECT EXPLAINS: command line arguments
 How to Write a program that prints its name, followed by a new line
 hoe to Write a program that prints the number of arguments passed into it.
+How to Write a program that prints all arguments it receives.
