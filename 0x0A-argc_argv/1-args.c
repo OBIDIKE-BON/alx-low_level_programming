4025c897c6f9545c9e46ebc8bf2d675d11f20230
obidikejekwu <obidike.jekwu@gmail.com>
2023-03-13 23:40:41 +0100

    Write a program that prints the number of arguments passed into it.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * Description: main - prints number of command line arguments.
+ * followed by a new line.
+ * @argc: the number of cammand line arguments passed at run-time.
+ * @argv: an array of cammand line arguments passed at run-time.
+ * Return: 0 if success.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+	UNUSED(argv);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 # THE PROJECT EXPLAINS: command line arguments
 How to Write a program that prints its name, followed by a new line
+hoe to Write a program that prints the number of arguments passed into it.
