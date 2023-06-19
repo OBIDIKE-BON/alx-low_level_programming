57c9c09218f93f1f30659c84fcfae591828d1c02
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-06-19 16:25:05 +0100

    Write a program that prints the name of the file it was compiled from, followed by a new line.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
 /**
+ * printString - Function to print a string using _putchar()
+ * @str: pointer to the char being printed
+ */
+
+void printString(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
+/**
  * main - Prgram Entry point and
  * Prints the name of this file
  */
 
 int main(void)
 {
-	printf("%s", __FILE__);
+	printString("%s", __FILE__);
 }
