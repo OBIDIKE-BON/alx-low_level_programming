280b91b6d1e5b54bbae16ccfb380c897a4960bbc
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-06-21 19:38:23 +0100

    Write a program that performs simple operations.

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,9 +21,8 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops++)
-	{
-		if (ops[i].op == s)
-			return (ops[i].f);
-	}
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
+
+	return (ops[i].f);
 }
