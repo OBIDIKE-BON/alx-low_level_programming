f774f3ccd2d14daa8253e5545b49f64abedd504a
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-07-05 21:12:28 +0100

    Write a function that Frees a linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,15 @@
 
 void free_listint2(listint_t **head)
 {
-	free(*head);
-	*head = NULL;
+	listint_t *temp;
+
+	if (!head)
+		return;
+	while (*head)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
 }
 
