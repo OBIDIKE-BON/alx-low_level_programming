84b4ee387bd0f30da7106d7947c21ba704f87a9f
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-17 13:45:22 +0100

    print a list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
-
+/**
+ * print_dlistint - print a list.
+ * @h: poiter to the list head.
+ * Return: number of elements in the list.
+*/
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
-	while (h->next)
+	while (h->prev)
+		h = h->prev;
+	
+	while (h)
 	{
 		printf("%d\n", h->n);
+		h = h->next;
 		++i;
 	}
 	return (i);
