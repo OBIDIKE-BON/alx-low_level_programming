14ac7933d123f4aa8890d8891c5fb414158a3d32
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-18 03:15:17 +0100

    insert node at x

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,6 +8,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
 
+	while (h->prev != NULL)
+		h = h->prev;
+
 	while (h)
 	{
 		printf("%d\n", h->n);
--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (*h);
 	}
 	while ((*h)->prev)
-		h = (*h)->prev;
+		*h = (*h)->prev;
 	while ((*h)->next)
 	{
 		if (index == idx)
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			(*h)->next = new;
 			return (new);
 		}
-		(*h) = (*h)->next;
+		*h = (*h)->next;
 		index++;
 	}
 	return (NULL);
