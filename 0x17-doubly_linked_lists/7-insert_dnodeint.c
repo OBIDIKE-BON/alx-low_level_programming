b7b4d10569caf21e637db4e30a6d0f832defd3f1
OBIDIKE-BON <bonifaceobidike@gmail.com>
2023-08-18 22:30:56 +0100

    out of range index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (*h == NULL)
+	if (*h == NULL && idx == 0)
 	{
 		new->next = NULL;
 		new->prev = NULL;
